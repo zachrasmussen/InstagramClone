5307370613fd6d87dd234e76d1f7b5a25b6f423b
Zach Rasmussen <zsrasmussen@gmail.com>
2023-09-05 11:32:55 -0600

    Added a login page

---
+++ InstagramClone/Core/Authentication/LoginView.swift
@@ -0,0 +1,121 @@
+//
+//  LoginView.swift
+//  InstagramClone
+//
+//  Created by Zachary Rasmussen on 9/5/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+                Spacer()
+                
+                // logo image
+                Image("bubble")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 220, height: 100)
+                    
+                // text field
+                VStack {
+                    TextField("Enter Your Email", text: $email)
+                        .autocapitalization(.none)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                    
+                    SecureField("Enter Your password", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+
+                }
+                
+                Button {
+                    print("Show forgot password")
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                        .padding(.trailing, 28)
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                
+                Button {
+                    print("Login")
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 360, height: 44)
+                        .background(Color(.systemBlue))
+                        .cornerRadius(8)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+                    
+                    Text("OR")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+                        .foregroundColor(.gray)
+                }
+                .foregroundColor(.gray)
+                
+                HStack {
+                    Image("party-1")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                    
+                    Text("Continue with Facebook")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(.systemBlue))
+                }
+                .padding(.top, 8)
+                
+                Spacer()
+                
+                Divider()
+                
+                NavigationLink {
+                    Text("Sign Up")
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account")
+                        
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                }
+                .padding(.vertical, 16)
+
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
