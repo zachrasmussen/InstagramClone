0dd3ea2efddd5df0cd1b91d8c47ab843031b2ab1
Zach Rasmussen <zsrasmussen@gmail.com>
2023-09-05 09:02:31 -0600

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Zachary Rasmussen on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
