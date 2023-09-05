60420990f2e61c306e7c00077f97b25f4208e7e0
Zach Rasmussen <zsrasmussen@gmail.com>
2023-09-05 11:46:23 -0600

    create an account page

---
+++ InstagramClone/Core/Authentication/AddEmailView.swift
@@ -0,0 +1,68 @@
+//
+//  AddEmailView.swift
+//  InstagramClone
+//
+//  Created by Zachary Rasmussen on 9/5/23.
+//
+
+import SwiftUI
+
+struct AddEmailView: View {
+    @State private var email = ""
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            Text("Add your email")
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding(.top)
+            
+            Text("You'll use this email to sign in to your account")
+                .font(.footnote)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 24)
+            
+            TextField("Email", text: $email)
+                .autocapitalization(.none)
+                .font(.subheadline)
+                .padding(12)
+                .background(Color(.systemGray6))
+                .cornerRadius(10)
+                .padding(.horizontal, 24)
+                .padding(.top)
+            
+            Button {
+                print("Login")
+            } label: {
+                Text("Login")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .frame(width: 360, height: 44)
+                    .background(Color(.systemBlue))
+                    .cornerRadius(8)
+            }
+            .padding(.vertical)
+            
+            Spacer()
+        }
+        
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Image(systemName: "chevron.left")
+                    .imageScale(.large)
+                    .onTapGesture {
+                        dismiss()
+                    }
+            }
+        }
+    }
+}
+
+struct AddEmailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddEmailView()
+    }
+}
--- InstagramClone/Core/Authentication/LoginView.swift
+++ InstagramClone/Core/Authentication/LoginView.swift
@@ -97,7 +97,8 @@ struct LoginView: View {
                 Divider()
                 
                 NavigationLink {
-                    Text("Sign Up")
+                    AddEmailView()
+                        .navigationBarBackButtonHidden(true)
                 } label: {
                     HStack(spacing: 3) {
                         Text("Don't have an account")
