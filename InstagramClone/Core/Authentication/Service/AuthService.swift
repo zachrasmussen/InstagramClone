7716e8f574c3d5b7a4d2682cd3a3ad352667d0cf
Zach Rasmussen <zsrasmussen@gmail.com>
2023-09-06 10:30:34 -0600

    added firebase packages

---
+++ InstagramClone/Core/Authentication/Service/AuthService.swift
@@ -0,0 +1,36 @@
+//
+//  AuthService.swift
+//  InstagramClone
+//
+//  Created by Zachary Rasmussen on 9/6/23.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthServices {
+    
+    @Published var userSession: FirebaseAuth.User?
+    
+    static let shared = AuthService()
+    
+    init() {
+        self.userSession = Auth.auth().currentUser
+    }
+    
+    func login(withEmail email: String, password: String) async throws {
+        
+    }
+    
+    func createUser(email:String, password: String, username: String) async throws {
+        
+    }
+    
+    func loadUserData() async throws {
+        
+    }
+    
+    func signout() {
+        
+    }
+}
---
+++ InstagramClone/Core/Root/ViewModel/ContentViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  ContentViewModel.swift
+//  InstagramClone
+//
+//  Created by Zachary Rasmussen on 9/6/23.
+//
+
+import Foundation
+import Firebase
+import Combine
+
+class ContentViewModel: ObservableObject {
+    
+    private let service = AuthService.shared
+    private var cancellables = Set<AnyCancellable>()
+    
+    @Published var userSession: FirebaseAuth.User?
+    
+    init() {
+        
+    }
+    
+    func setupSubscribers() {
+        service.$userSession.sink { [weak self] userSession in
+            self?.userSession = userSession
+        }
+        .store(in: &cancellables)
+    }
+}
