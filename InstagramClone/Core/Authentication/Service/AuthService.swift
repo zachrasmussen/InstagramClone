a2db6377ef3356f9ef6b6c1df4b01df1360e589e
Zach Rasmussen <zsrasmussen@gmail.com>
2023-09-06 12:29:09 -0600

    created login functionality in AuthService

--- InstagramClone/Core/Authentication/Service/AuthService.swift
+++ InstagramClone/Core/Authentication/Service/AuthService.swift
@@ -19,7 +19,12 @@ class AuthService {
     }
     
     func login(withEmail email: String, password: String) async throws {
-        
+        do {
+            let result = try await Auth.auth().signIn(withEmail: email, password: password)
+            self.userSession = result.user
+        } catch {
+            print("DEBUG: Failed to log in user with error \(error.localizedDescription)")
+        }
     }
     
     func createUser(email:String, password: String, username: String) async throws {
