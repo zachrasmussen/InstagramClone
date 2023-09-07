bc4cfb0f5c005af72b78e61501e9bdb49b19cef2
Zach Rasmussen <zsrasmussen@gmail.com>
2023-09-07 12:23:06 -0600

    posts in feed

--- InstagramClone/Core/Feed/ViewModel/FeedViewModel.swift
+++ InstagramClone/Core/Feed/ViewModel/FeedViewModel.swift
@@ -11,7 +11,20 @@ import Firebase
 class FeedViewModel: ObservableObject {
     @Published var posts = [Post]()
     
+    init() {
+        Task { try await fetchPosts() }
+    }
+    
+    @MainActor
     func fetchPosts() async throws {
         let snapshot = try await Firestore.firestore().collection("posts").getDocuments()
+        self.posts = try snapshot.documents.compactMap({ try $0.data(as: Post.self) })
+        
+        for i in 0 ..< posts.count {
+            let post = posts[i]
+            let ownerUid = post.ownerUid
+            let postUser = try await UserService.fetchUser(withUid: ownerUid)
+            posts[i].user = postUser
+        }
     }
 }
