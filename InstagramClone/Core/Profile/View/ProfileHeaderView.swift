e85562dbc677f1ca6815390a95409972dd72b4a9
Zach Rasmussen <zsrasmussen@gmail.com>
2023-09-18 11:32:43 -0600

    follow button (UI changes)

--- InstagramClone/Core/Profile/View/ProfileHeaderView.swift
+++ InstagramClone/Core/Profile/View/ProfileHeaderView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ProfileHeaderView: View {
-    let user: User
     @ObservedObject var viewModel: ProfileViewModel
     @State private var showEditProfile = false
+    
+    private var user: User {
+        return viewModel.user
+    }
+    
+    private var isFollowed: Bool {
+        return user.isFollowed ?? false
+    }
+    
+    private var buttonTitle: String {
+        if user.isCurrentUser {
+            return "Edit Profile"
+        } else {
+            return isFollowed ? "Following" : "Follow"
+        }
+    }
+    
+    private var buttonBackgroundColor: Color {
+        if user.isCurrentUser || isFollowed {
+            return .white
+        } else {
+            return Color(.systemBlue)
+        }
+    }
+    
+    private var buttonForegroundColor: Color {
+        if user.isCurrentUser || isFollowed {
+            return .black
+        } else {
+            return .white
+        }
+    }
+    
+    private var buttonBorderColor: Color {
+        if user.isCurrentUser || isFollowed {
+            return .gray
+        } else {
+            return .clear
+        }
+    }
     
     init(user: User) {
         self.viewModel = ProfileViewModel(user: user)
@@ -58,19 +97,19 @@ struct ProfileHeaderView: View {
                 if user.isCurrentUser {
                     showEditProfile.toggle()
                 } else {
-                    print("Follow user")
+                    handleFollowTapped()
                 }
             } label: {
-                Text(user.isCurrentUser ? "Edit Profile" : "Follow")
+                Text(buttonTitle)
                     .font(.subheadline)
                     .fontWeight(.semibold)
                     .frame(width: 360, height: 32)
-                    .background(user.isCurrentUser ? .white : Color(.systemBlue))
-                    .foregroundColor(user.isCurrentUser ? .black : .white)
+                    .background(buttonBackgroundColor)
+                    .foregroundColor(buttonForegroundColor)
                     .foregroundColor(.black)
                     .cornerRadius(6)
                     .overlay(RoundedRectangle(cornerRadius: 6)
-                        .stroke(user.isCurrentUser ? .gray : .clear, lineWidth: 1)
+                        .stroke(buttonBorderColor, lineWidth: 1)
                     )
                 
             }
@@ -79,6 +118,14 @@ struct ProfileHeaderView: View {
         }
         .fullScreenCover(isPresented: $showEditProfile) {
             EditProfileView(user: user)
+        }
+    }
+    
+    func handleFollowTapped() {
+        if isFollowed {
+            viewModel.unfollow()
+        } else {
+            viewModel.follow()
         }
     }
 }
