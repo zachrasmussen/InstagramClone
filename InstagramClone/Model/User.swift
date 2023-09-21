bd44797c723f9f057b473ee5630a80d85c07c899
Zach Rasmussen <zsrasmussen@gmail.com>
2023-09-21 16:10:21 -0600

    user stats

--- InstagramClone/Model/User.swift
+++ InstagramClone/Model/User.swift
@@ -17,11 +17,18 @@ struct User: Identifiable, Hashable, Codable {
     let email: String
     
     var isFollowed: Bool? = false
+    var stats: UserStats?
     
     var isCurrentUser: Bool {
         guard let currentUid = Auth.auth().currentUser?.uid else { return false }
         return currentUid == id
     }
+}
+
+struct UserStats: Codable, Hashable {
+    var followingCount: Int
+    var followersCount: Int
+    var postsCount: Int
 }
 
 extension User {
--- InstagramClone/Services/UserService.swift
+++ InstagramClone/Services/UserService.swift
@@ -87,3 +87,35 @@ extension UserService {
         return snapshot.exists
     }
 }
+
+// User Stats
+
+extension UserService {
+    static func fetchUserStats(uid: String) async throws -> UserStats {
+        async let followingSnapshot = try await FirebaseConstants
+            .FollowingCollection
+            .document(uid)
+            .collection("user-following")
+            .getDocuments()
+        
+        let followingCount = try await followingSnapshot.count
+        
+        async let followersSnapshot = try await FirebaseConstants
+            .FollowersCollection
+            .document(uid)
+            .collection("user-followers")
+            .getDocuments()
+        
+        let followerCount = try await followersSnapshot.count
+        
+        async let postSnapshot = try await FirebaseConstants
+            .PostCollection
+            .whereField("ownerUid", isEqualTo: uid)
+            .getDocuments()
+        
+        let postsCount = try await postSnapshot.count
+        
+        print("DEBUG: Did call fetch user stats")
+        return .init(followingCount: followingCount, followersCount: followerCount, postsCount: postsCount)
+    }
+}
